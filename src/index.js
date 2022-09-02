b44c1e178843b80e6311f03cbf077cf2258fd490
Gulab <sujeetsinghsikarwar2001@gmail.com>
2022-09-02 08:16:19 +0530

    started   routing feature in ROUTING BRANCH

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  {store} from './stateManagement/store'
+import { store } from './stateManagement/store'
 
 import { Provider } from 'react-redux';
+import  {}  from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
 
     </Provider>
 
